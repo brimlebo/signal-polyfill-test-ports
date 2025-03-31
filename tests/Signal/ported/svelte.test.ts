0c6671424a049749b2d021cd9009e3eee31aecc7
Brian.Jensvoll-Jorgensen <brian.jensvoll-jorgensen@student.uib.no>
2025-03-31 19:06:36 +0200

    Used the effect flush from the watcher test to force effect to go through pending

--- tests/Signal/ported/svelte.test.ts
+++ tests/Signal/ported/svelte.test.ts
@@ -1,50 +1,61 @@
-import { describe, it, assert, expect, vi } from 'vitest';
+import { describe, test, it, assert, expect, vi, afterEach } from 'vitest';
 import { Signal } from '../../../src';
-import { effect } from './effect';
 
 
 describe('Ported - Svelte', () => {
     // Naming kind of weird at the moment because I dont know what to rename them to
-    
+
+    // Effect implementation taken from watcher.test.ts
+    type Destructor = () => void;
+    const notifySpy = vi.fn();
+
+    const watcher = new Signal.subtle.Watcher(() => {
+        notifySpy();
+    });
+
+    function effect(cb: () => Destructor | void): () => void {
+        let destructor: Destructor | void;
+        const c = new Signal.Computed(() => (destructor = cb()));
+        watcher.watch(c);
+        c.get();
+        return () => {
+            destructor?.();
+            watcher.unwatch(c);
+        };
+    }
+
+    // Forces tasks to be run
+    function flushPending() {
+        for (const signal of watcher.getPending()) {
+            signal.get();
+        }
+
+        expect(watcher.getPending()).toStrictEqual([]);
+    }
+
+    afterEach(() => watcher.unwatch(...Signal.subtle.introspectSources(watcher)));
+
     // Currently fails, effect currently only get and log the initial values, not subsequent updates to the state signal
     // https://github.com/sveltejs/svelte/blob/04257925d22d8ecef37f50330ac258c7f97dca0c/packages/svelte/tests/signals/test.ts#L54
-    it('effect with state and derived in it', () => {
+    test('effect with state and derived (computed) in it', () => {
 		const log: string[] = [];
 
 		let count = new Signal.State(0);
 
 		let double = new Signal.Computed(() => count.get() * 2);
-
-        /* let w = new Signal.subtle.Watcher(() => {
-            log.push(`${count.get()}:${double.get()}`)
-        });
-
-        w.watch(double) */
         
         effect(() => {
             log.push(`${count.get()}:${double.get()}`)
         });
         
         count.set(1);
+
+        flushPending();
+
         count.set(2);
 
+        flushPending();
+
         assert.deepEqual(log, ['0:0', '1:2', '2:4']);
-
-        /* w.unwatch(double) */
 	});
-
-    // Something to test if effect() works as I want
-    it('teeeeeeeeeests', () => {
-        let counter = new Signal.State(0)
-        counter.set(1)
-
-        // Should in theory run until "infinitly" until the counter is 1000
-        effect(() => {
-            if (counter.get() !== 1000) {
-                counter.set(counter.get() + 1)
-            }
-        });
-
-        assert.equal(1000, counter.get())
-    });
 });
